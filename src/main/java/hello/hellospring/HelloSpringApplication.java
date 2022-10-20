38a888ac086393846b18ae7ed44d886314d9e018
itsds1124@gmail.com <itsds124@gmail.com>
2022-10-20 22:51:38 +0900

    basic 1

---
+++ src/main/java/hello/hellospring/HelloSpringApplication.java
@@ -0,0 +1,14 @@
+package hello.hellospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloSpringApplication.class, args);
+	}
+
+}
+//springboot tomcat이나 webserver내장 클래스에서 어노테이션 실행
